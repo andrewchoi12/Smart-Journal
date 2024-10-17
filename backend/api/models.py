301d58ffa7ab6549fcd94cc711cb482247375a6c
Andrew Choi <78119382+andrewchoi12@users.noreply.github.com>
2024-10-17 15:50:59 -0400

    Added Note model + serializer
    Also some views and urls

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Note model
+class Note(models.Model):
+    # Define the fields in Python
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # We don't pass this, just automatically create on creation
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Foreign key links a user with data that belongs
+    # to that user
+    #
+    # CASCADE just means delete all notes associated when user is deleted
+    # Related name tells us what field name we put on the user
+    # that references all of its notes (----.notes)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    
+    def __str__(self):
+        return self.title
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    # List of all diff objects we look at
+    # to make sure we don't create a duplicate
+    queryset = User.objects.all()
+
+    # Tells the view what kind of data
+    # we need to accept to make a new user
+    serializer_class = UserSerializer
+
+    # Specifies who can actually call this
+    # In this case anyone
+    permission_classes = [AllowAny]

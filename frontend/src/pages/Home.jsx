5afe18842b1548be5333b964caeb8d1b3fec3ec8
Andrew Choi <78119382+andrewchoi12@users.noreply.github.com>
2024-10-22 18:13:46 -0400

    Adding Home Page api calls

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,41 @@
+import { useState, useEffect } from "react"
+import api from "../api"
+
 function Home() {
+    const [notes, setNotes] = useState([])
+    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        getNotes();
+    }, [])
+
+    const getNotes = () => {
+        // Gives all notes user has written
+        api
+            .get("/api/notes/")
+            .then((res) => res.data)
+            .then((data) => { setNotes(data); console.log(data) })
+            .catch((err) => alert(err));
+    };
+
+    const deleteNote = (id) => {
+        api.delete(`/api/notes/delete/${id}/`).then((res) => {
+            if (res.status === 204) alert("Note Deleted!")
+            else alert("Failed to delete note.")
+        }).catch((error) => alert(error))
+        getNotes()
+    }
+
+    const createNote = (e) => {
+        e.preventDefault()
+        api.post("/api/notes/", {content, title}).then((res) => {
+            if (res.status === 201) alert("Note Created!")
+            else alert("Failed to make create note.")
+        }).catch((error) => alert(error))
+        getNotes()
+    }
+
     return <div>Home</div>
 }
 
